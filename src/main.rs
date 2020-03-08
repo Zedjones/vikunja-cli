cde13b50027777ad4b0e4d26cfc28b96c68abb59
Zedjones <dojoman19@gmail.com>
2020-03-07 22:00:16 -0500

    Switched to beta of clap and started creating opts struct

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 mod api;
 
-use clap::{App, load_yaml};
+use clap::{load_yaml, Clap};
 use api::Client;
+
+#[derive(Clap)]
+struct Opts {
+    username: String,
+    password: String,
+    server: String
+}
 
 fn main() {
     let _yaml = load_yaml!("cli.yml");
