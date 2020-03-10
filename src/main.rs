0510b56fbcb3e3ce5827943ec344e3e40b35d5fb
Zedjones <dojoman19@gmail.com>
2020-03-10 16:36:39 -0400

    Got rid of some old print statements

--- src/main.rs
+++ src/main.rs
@@ -14,9 +14,7 @@ struct Config {
 fn main() {
     dotenv::dotenv().unwrap();
     let config = envy::from_env::<Config>().unwrap();
-    println!("{:?}", config);
     let client = Client::new(&config.server, &config.username, &config.password);
-    println!("{:?}", client);
     let client_val = client.unwrap();
     println!("{:?}", client_val.get_tasks_info());
 }
