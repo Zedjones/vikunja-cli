4c88a6fa884ca11b1ee91a178d4f33c91e28f54f
Zedjones <dojoman19@gmail.com>
2020-03-22 11:20:27 -0400

    Moved running the TUI into a separate function entirely

--- src/frontend/mod.rs
+++ src/frontend/mod.rs
@@ -4,8 +4,9 @@ use cursive_async_view::AsyncView;
 use cursive::views::{Dialog, ScrollView, TextView};
 use cursive::Cursive;
 
-pub fn get_view(client: Client) -> AsyncView<_> {
-    AsyncView::new_with_bg_creator(&mut siv, move || {
+pub fn run_ui(client: Client) {
+    let mut siv = Cursive::default();
+    let async_view = AsyncView::new_with_bg_creator(&mut siv, move || {
         // this function is executed in a background thread, so we can block
         // here as long as we like
         let all_info = client.get_all_info().unwrap();
@@ -13,4 +14,8 @@ pub fn get_view(client: Client) -> AsyncView<_> {
         // enough blocking, let's show the content
         Ok(format!("{:?}", all_info.tasks))
     }, |text| ScrollView::new(Dialog::around(TextView::new(text)).button("Quit", |s| s.quit())));
+        
+    siv.add_layer(async_view);
+
+    siv.run();
 }
--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,6 @@ mod config;
 mod frontend;
 
 use api::*;
-use cursive::{Cursive, views::{Dialog, TextView, ScrollView}};
-use cursive_async_view::AsyncView;
 use config::Config;
 use frontend::*;
 
@@ -14,17 +12,5 @@ fn main() {
     let client = Client::new(&config.server, &config.username, &config.password);
     let client_val = client.unwrap();
 
-    let mut siv = Cursive::default();
-    let async_view = AsyncView::new_with_bg_creator(&mut siv, move || {
-        // this function is executed in a background thread, so we can block
-        // here as long as we like
-        let all_info = client_val.get_all_info().unwrap();
-    
-        // enough blocking, let's show the content
-        Ok(format!("{:?}", all_info.tasks))
-    }, |text| ScrollView::new(Dialog::around(TextView::new(text)).button("Quit", |s| s.quit())));
-    
-    siv.add_layer(async_view);
-
-    siv.run();
+    run_ui(client_val)
 }
