7551369a3a9cef80f66c8cf5eeae10c297776508
Zedjones <dojoman19@gmail.com>
2020-03-22 22:54:19 -0400

    Got basic list of lists working

--- src/frontend/mod.rs
+++ src/frontend/mod.rs
@@ -1,21 +1,29 @@
-use super::api::Client;
+use super::api::{Client, List, FullInfo};
 
 use cursive_async_view::AsyncView;
-use cursive::views::{Dialog, ScrollView, TextView};
+use cursive::views::{Dialog, TextView, Button, ListView};
+use cursive::view::IntoBoxedView as _;
 use cursive::Cursive;
 
 pub fn run_ui(client: Client) {
     let mut siv = Cursive::default();
     let async_view = AsyncView::new_with_bg_creator(&mut siv, move || {
-        // this function is executed in a background thread, so we can block
-        // here as long as we like
         let all_info = client.get_all_info().unwrap();
-    
-        // enough blocking, let's show the content
-        Ok(format!("{:?}", all_info.tasks))
-    }, |text| ScrollView::new(Dialog::around(TextView::new(text)).button("Quit", |s| s.quit())));
+
+        Ok(all_info)
+    }, |full_info| Dialog::around(make_list_buttons(full_info)).button("Quit", |s| s.quit()));
         
     siv.add_layer(async_view);
 
     siv.run();
+}
+
+fn make_list_buttons(all_info: FullInfo) -> ListView {
+    let mut list_view = ListView::new();
+    all_info.lists
+        .iter()
+        .map(|list| Button::new_raw(&list.title, |s| s.quit()))
+        .map(|button| button.as_boxed_view())
+        .for_each(|button_view| list_view.add_child("", button_view));
+    list_view
 }
