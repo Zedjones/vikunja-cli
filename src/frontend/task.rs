5f203470185a97c9931da3306f1d402db61736fc
Zedjones <dojoman19@gmail.com>
2020-03-29 22:28:35 -0400

    Continued working on handle popup for adding a task

--- src/frontend/task.rs
+++ src/frontend/task.rs
@@ -1,7 +1,7 @@
 use super::Client;
 
 use cursive::views::{Dialog, Button, ListView, EditView};
-use cursive_async_view::AsyncView;
+//use cursive_async_view::AsyncView;
 
 pub fn add_task_view(client: &mut Client, list_name: &str) -> cursive::views::Dialog {
     Dialog::new()
@@ -16,6 +16,9 @@ fn handle_adding(client: &mut Client, list_name: &str, s: &mut cursive::Cursive,
         s.add_layer(Dialog::info("Please enter a name for the task."));
     }
     else {
-        
+        match client.add_task(list_name, task) {
+            Err(error) => s.add_layer(Dialog::info(error)),
+            _ => s.add_layer(Dialog::info(format!("Successfully added task to {}", list_name)))
+        };
     }
 }
