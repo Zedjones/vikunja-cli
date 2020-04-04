ba7b810e4992b4c0dce974f8dbea64b650d4a5a3
Zedjones <dojoman19@gmail.com>
2020-04-03 21:08:17 -0400

    Still working

--- src/frontend/mod.rs
+++ src/frontend/mod.rs
@@ -1,7 +1,10 @@
 mod task;
 
 use super::api::{Client, FullInfo};
+use task::add_task_view;
 
+use std::sync::Arc;
+use std::cell::RefCell;
 use cursive_async_view::AsyncView;
 use cursive::views::{Dialog, Button, ListView};
 use cursive::view::IntoBoxedView as _;
@@ -9,22 +12,29 @@ use cursive::Cursive;
 
 pub fn run_ui(client: Client) {
     let mut siv = Cursive::default();
+    let client_cell = Arc::new(RefCell::from(client));
+    let client_copy = client_cell.clone();
     let async_view = AsyncView::new_with_bg_creator(&mut siv, move || {
-        let all_info = client.get_all_info().unwrap();
+        let all_info = client_copy.borrow().get_all_info().unwrap();
+        let copy = client_cell.clone();
 
         Ok(all_info)
-    }, |full_info| Dialog::around(make_list_buttons(full_info)).button("Quit", |s| s.quit()));
+    }, move |full_info| Dialog::around(make_list_buttons(client_cell, full_info)).button("Quit", |s| s.quit()));
         
     siv.add_layer(async_view);
 
     siv.run();
 }
 
-fn make_list_buttons(all_info: FullInfo) -> ListView {
+fn make_list_buttons(client: Rc<RefCell<Client>>, all_info: FullInfo) -> ListView {
     let mut list_view = ListView::new();
     all_info.lists
         .iter()
-        .map(|list| Button::new_raw(&list.title, |s| s.quit()).as_boxed_view())
+        .map(|list| Button::new_raw(&list.title, move |s| {
+            let client_clone = client.clone();
+            let title = Rc::new(RefCell::from((&list.title).clone()));
+            s.add_layer(add_task_view(client_clone, title))
+        }).as_boxed_view())
         .for_each(|button_view| list_view.add_child("", button_view));
     list_view
 }
--- src/frontend/task.rs
+++ src/frontend/task.rs
@@ -1,25 +1,27 @@
 use super::Client;
 
 use cursive::views::{Dialog, Button, ListView, EditView};
+use std::cell::RefCell;
+use std::rc::Rc;
 //use cursive_async_view::AsyncView;
 
-pub fn add_task_view(client: &mut Client, list_name: &str) -> cursive::views::Dialog {
+pub fn add_task_view(client: Rc<RefCell<Client>>, list_name: Rc<RefCell<String>>) -> cursive::views::Dialog {
     Dialog::new()
         .title("Add task")
         .content(
             EditView::new()
-                    .on_submit(|s, text| handle_adding(client, list_name, s, text))
+                    .on_submit(move |s, text| handle_adding(client.clone(), list_name.clone(), s, text))
         )
 }
 
-fn handle_adding(client: &mut Client, list_name: &str, s: &mut cursive::Cursive, task: &str) {
+fn handle_adding(client: Rc<RefCell<Client>>, list_name: Rc<RefCell<String>>, s: &mut cursive::Cursive, task: &str) {
     if task.is_empty() {
         s.add_layer(Dialog::info("Please enter a name for the task."));
     }
     else {
-        match client.add_task(list_name, task) {
+        match client.borrow().add_task(&list_name.borrow(), task) {
             Err(error) => s.add_layer(Dialog::info(error)),
-            _ => s.add_layer(Dialog::info(format!("Successfully added task to {}", list_name)))
+            _ => s.add_layer(Dialog::info(format!("Successfully added task to {}", &list_name.borrow())))
         };
     }
 }
